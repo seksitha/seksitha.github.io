a533b8edd3fa0be1d78b30337db860f9821b3678
Sek Sitha <seksitha@gmail.com>
2021-12-22 15:24:24 +0700

    change chanel

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
 const SCOPES = 'https://www.googleapis.com/auth/youtube';
+const defaultChannel = 'Sek Sitha';
 
 const authorizeButton = document.getElementById('authorize-button');
 const signoutButton = document.getElementById('signout-button');
@@ -12,7 +13,6 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
 
 // Form submit and change channel
 channelForm.addEventListener('submit', e => {
